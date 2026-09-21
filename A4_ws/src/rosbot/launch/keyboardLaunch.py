01aa2bd27d69b40ef3b20a1a1117f830071d3ca9
Arael Anaya <arael.a.anaya@gmail.com>
2026-09-21 09:33:11 -0600

    Add rosbot package from lab1

---
+++ A4_ws/src/rosbot/launch/keyboardLaunch.py
@@ -0,0 +1,37 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+import launch
+import launch_ros.actions
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    bringup_pkg_path = get_package_share_directory('bringup')
+
+    teleop_twist_keyboard_node = launch_ros.actions.Node(
+                package='teleop_twist_keyboard',
+                executable='teleop_twist_keyboard',
+                name='teleop_twist_keyboard',
+                prefix=['xterm -e'],
+                output='screen')
+
+
+    bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_pkg_path, 'launch/bringup.launch.py')),
+    )
+
+    return launch.LaunchDescription([
+        teleop_twist_keyboard_node,
+        bringup_launch,
+    ])
+
+
+if __name__ == '__main__':
+    # Create a LaunchDescription object
+    ld = generate_launch_description()
+
+    ls = launch.LaunchService()
+    ls.include_launch_description(ld)
+    ls.run()
---
+++ A7_ws/src/rosbot/rosbot/teleop_joy.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+
+class teleopJoy(Node):
+    def __init__(self):
+        self.MAX_SPEED = 5.0
+        self.MAX_TURN = 5.0
+
+        super().__init__("teleop_joy")
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel' , 10)
+        self.poseSubscription = self.create_subscription(
+            Joy, 'ros_robot_controller/joy' , self.joyCallback, 10)
+
+
+    def joyCallback(self,msg):
+        axes = msg.axes
+        leftStickY = axes[1]
+        rightStickX = axes[2]
+        buttons = msg.buttons
+
+        speedCMD = Twist()
+
+        speedCMD.linear.x = leftStickY * self.MAX_SPEED
+        speedCMD.angular.z = rightStickX * self.MAX_TURN
+
+        self.publisher_.publish(speedCMD)
+        
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = teleopJoy()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
